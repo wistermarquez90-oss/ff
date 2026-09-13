08ab1680e31d85cfdb998afc459e19f0fd871fd9
wistermarquez90-oss <wistermarquez90@gmail.com>
2026-09-13 17:25:07 -0400

    jkl

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://github.com/wistermarquez90-oss',
-  base: '/landing-moreka',
+  site: 'https://wistermarquez90-oss.github.io',
+  base: '/ff',
   integrations: [tailwind()]
 });
